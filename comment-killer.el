20464e5bcaf30fd61625e56282a5ba6720ac2cdc
Calvin Beck <hobbes@ualberta.ca>
2020-07-09 18:18:49 -0400

    Some elisp tweaks.

--- comment-killer.el
+++ comment-killer.el
@@ -1,10 +1,15 @@
 (defun remove-comments (f)
   (save-excursion
     (find-file f)
-    (comment-kill 100000)
+    (comment-kill 10000)
     (write-file f)))
 
 (defun coq-comment-killer (dir)
   (interactive "DDirectory for files:")
   (let ((files (directory-files-recursively dir ".*\\.v$" nil)))
+    (mapcar (lambda (f) (remove-comments f)) files)))
+
+(defun ml-comment-killer (dir)
+  (interactive "DDirectory for files:")
+  (let ((files (directory-files-recursively dir ".*\\.ml$" nil)))
     (mapcar (lambda (f) (remove-comments f)) files)))

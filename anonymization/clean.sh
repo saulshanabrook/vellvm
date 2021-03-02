013596e423e8df0c13d5a0182485b31cc5f70545
Calvin Beck <hobbes@ualberta.ca>
2021-03-02 15:52:02 -0500

    Clean script to rename vellvm -> vir everywhere.

--- anonymization/clean.sh
+++ anonymization/clean.sh
@@ -2,3 +2,6 @@
 
 find .. -name "*vellvm*" -exec rename vellvm vir {} \;
 find .. -name "*Vellvm*" -exec rename Vellvm Vir {} \;
+find .. -not -path "*anonymization*" -not -path "*.git*" -type f -exec sed -i -e "s/vellvm/vir/g" {} \;
+find .. -not -path "*anonymization*" -not -path "*.git*" -type f -exec sed -i -e "s/Vellvm/Vir/g" {} \;
+find .. -not -path "*anonymization*" -not -path "*.git*" -type f -exec sed -i -e "s/VELLVM/VIR/g" {} \;

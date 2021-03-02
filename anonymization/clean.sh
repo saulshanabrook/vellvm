3397e2adac97ab5dcc6777ebf6227a6c20d6ae40
Calvin Beck <hobbes@ualberta.ca>
2021-03-02 15:55:05 -0500

    Script to clean vellvm copyrights.

--- anonymization/clean.sh
+++ anonymization/clean.sh
@@ -5,3 +5,4 @@ find .. -name "*Vellvm*" -exec rename Vellvm Vir {} \;
 find .. -not -path "*anonymization*" -not -path "*.git*" -type f -exec sed -i -e "s/vellvm/vir/g" {} \;
 find .. -not -path "*anonymization*" -not -path "*.git*" -type f -exec sed -i -e "s/Vellvm/Vir/g" {} \;
 find .. -not -path "*anonymization*" -not -path "*.git*" -type f -exec sed -i -e "s/VELLVM/VIR/g" {} \;
+find .. -not -path "*anonymization*" -not -path "*.git*" -type f -exec sed -i -e "s/Copyright (c) 2017 Steve Zdancewic <stevez@cis.upenn.edu>/Anonymized/g" {} \;
